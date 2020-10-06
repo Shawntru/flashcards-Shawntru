8eecca5a8c497ec95dcf49255bc2a6b9bdff5983
Shawn Truesdale <shawnctruesdale@gmail.com>
2020-10-05 21:13:59 -0600

    Add addn testing for Turn, returnGuess function

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,17 +2,41 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
+  let card;
+  let turn;
+
+  beforeEach(function() {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    turn = new Turn('user guess', card);
+  });
+
   it('should be a function', function() {
-    const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', function() {
-    const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should contain a users quess', function() {
+    expect(turn.guess).to.deep.equal('user guess');
+  });
+
+  it('should contain the card being referenced', function() {
+    expect(turn.card).to.deep.equal({
+      id: 1,
+      question: 'What allows you to define a set of related information using key-value pairs?',
+      answers: [ 'object', 'array', 'function' ],
+      correctAnswer: 'object'
+    });
+  });
+
+  it('should be able to return the users guess', function() {
+    expect(turn.returnGuess()).to.deep.equal('user guess');
   });
 
 });
