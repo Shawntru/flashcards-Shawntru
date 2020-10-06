598c97fa13a66a20eff5c8cc333e19586340c30e
Shawn Truesdale <shawnctruesdale@gmail.com>
2020-10-05 21:34:57 -0600

    Complete IT1, finish testing for Card, Turn

--- src/Turn.js
+++ src/Turn.js
@@ -7,6 +7,20 @@ class Turn {
   returnGuess() {
     return this.guess;
   }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    if (this.guess === this.card.correctAnswer) return true;
+    else return false;
+  }
+
+  giveFeedback() {
+    if (this.evaluateGuess()) return 'correct!';
+    else return 'incorrect!';
+  }
 }
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -11,7 +11,7 @@ describe('Turn', function() {
 
   beforeEach(function() {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    turn = new Turn('user guess', card);
+    turn = new Turn('object', card);
   });
 
   it('should be a function', function() {
@@ -23,7 +23,7 @@ describe('Turn', function() {
   });
 
   it('should contain a users quess', function() {
-    expect(turn.guess).to.deep.equal('user guess');
+    expect(turn.guess).to.deep.equal('object');
   });
 
   it('should contain the card being referenced', function() {
@@ -36,7 +36,28 @@ describe('Turn', function() {
   });
 
   it('should be able to return the users guess', function() {
-    expect(turn.returnGuess()).to.deep.equal('user guess');
+    expect(turn.returnGuess()).to.deep.equal('object');
+  });
+
+  it('should be able to return the current card', function() {
+    expect(turn.returnCard()).to.deep.equal({
+      id: 1,
+      question: 'What allows you to define a set of related information using key-value pairs?',
+      answers: [ 'object', 'array', 'function' ],
+      correctAnswer: 'object'
+    });
+  });
+
+  it('should be able to evaluate the guess and return a true of false value', function() {
+    expect(turn.evaluateGuess()).to.deep.equal(true);
+    turn.guess = 'array';
+    expect(turn.evaluateGuess()).to.deep.equal(false);
+  });
+
+  it('should give feedback based on the guess', function () {
+    expect(turn.giveFeedback()).to.deep.equal('correct!');
+    turn.guess = 'array';
+    expect(turn.giveFeedback()).to.deep.equal('incorrect!');
   });
 
 });
